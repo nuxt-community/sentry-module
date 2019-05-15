db20b67ad5073abc44dcca5ab74f2474c0dc9b95
simon <s.tretter@gmail.com>
2019-05-16 00:09:09 +0200

    feat: add typescript types for $sentry
    env parameter SENTRY_DISABLED=true omits injection sentry at all, therefore checking if sentry is avaialble is the safer appraoch (and therefore making typedef of $sentry optional)

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,20 +3,24 @@ import { Client } from '@sentry/types';
 // add type to Vue context
 declare module 'vue/types/vue' {
   interface Vue {
-    $sentry: Client;
+    readonly $sentry?: Client;
   }
 }
 
-// since nuxt 2.7.1 there is "NuxtAppOptions" for app context - see https://github.com/nuxt/nuxt.js/pull/5701
-declare module '@nuxt/vue-app' {
+// App Context and NuxtAppOptions
+declare module '@nuxt/types' {
+  interface Context {
+    readonly $sentry?: Client;
+  }
+
   interface NuxtAppOptions {
-    $sentry: Client;
+    readonly $sentry?: Client;
   }
 }
 
 // add types for Vuex Store
 declare module 'vuex/types' {
   interface Store<S> {
-    $sentry: Client;
+    readonly $sentry?: Client;
   }
 }
