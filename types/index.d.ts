c96a3b7e96ce481ca5e6ea173649f8ec1318cf65
simon <s.tretter@gmail.com>
2019-05-16 12:32:31 +0200

    fix(ts): extend interfaces of places of inject()

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,22 @@
 import { Client } from '@sentry/types/dist/client';
 
+// add type to Vue context
 declare module "vue/types/vue" {
   interface Vue {
+    $sentry: Client;
+  }
+}
+
+// since nuxt 2.7.1 there is "NuxtAppOptions" for app context - see https://github.com/nuxt/nuxt.js/pull/5701
+declare module '@nuxt/vue-app' {
+  interface NuxtAppOptions {
+    $sentry: Client;
+  }
+}
+
+// add types for Vuex Store
+declare module 'vuex/types' {
+  interface Store<S> {
     $sentry: Client;
   }
 }
