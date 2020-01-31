c6420317d0e70e53854ce5d67bcb3a1348752871
Rafał Chłodnicki <rchl2k@gmail.com>
2020-01-31 12:58:42 +0100

    fix(types): Make $sentry non-optional since the Proxy is now used when disabled

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,24 +3,24 @@ import { Client } from '@sentry/types';
 // add type to Vue context
 declare module 'vue/types/vue' {
   interface Vue {
-    readonly $sentry?: Client;
+    readonly $sentry: Client;
   }
 }
 
 // App Context and NuxtAppOptions
 declare module '@nuxt/types' {
   interface Context {
-    readonly $sentry?: Client;
+    readonly $sentry: Client;
   }
 
   interface NuxtAppOptions {
-    readonly $sentry?: Client;
+    readonly $sentry: Client;
   }
 }
 
 // add types for Vuex Store
 declare module 'vuex/types' {
   interface Store<S> {
-    readonly $sentry?: Client;
+    readonly $sentry: Client;
   }
 }
