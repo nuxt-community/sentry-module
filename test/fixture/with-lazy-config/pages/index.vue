fc9309ac849bf3200bfd3379be66de181db206b4
pimlie <pimlie@hotmail.com>
2020-07-23 01:23:11 +0200

    chore: clarify error message

--- test/fixture/with-lazy-config/pages/index.vue
+++ test/fixture/with-lazy-config/pages/index.vue
@@ -24,7 +24,7 @@ export default {
     try {
       this.$sentry.captureEvent({ message: 'This should fail' })
       // eslint-disable-next-line no-console
-      console.error('The call to captureEvent should fail so this line shouldnt be printed')
+      console.error('The call to captureEvent should fail so this line shouldnt be printed (on initial load only, this line will be printed on eg HMR)')
     } catch (err) {
       // eslint-disable-next-line no-console
       console.info('Catched expected error on $sentry.captureEvent')
