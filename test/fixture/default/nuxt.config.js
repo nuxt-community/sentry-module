22049748399bd188b9c0c10fda44cb03f44bfcb6
Rafal Chlodnicki <rchl2k@gmail.com>
2021-08-19 15:31:16 +0200

    improve coverage?

--- test/fixture/default/nuxt.config.js
+++ test/fixture/default/nuxt.config.js
@@ -16,10 +16,14 @@ const config = {
   ],
   sentry: {
     dsn: 'https://fe8b7df6ea7042f69d7a97c66c2934f7@sentry.io.nuxt/1429779',
-    config: {},
     clientIntegrations: {
       // Integration from @Sentry/browser package.
       TryCatch: { eventTarget: false }
+    },
+    publishRelease: {
+      authToken: 'fakeToken',
+      org: 'MyCompany',
+      project: 'TestProject'
     }
   }
 }
