3603dc637b4761214e38b15e821e930d0f541d99
Rafal Chlodnicki <rchl2k@gmail.com>
2023-03-23 23:30:33 +0100

    fix(tracing): connect backend and frontend traces

--- src/templates/plugin.server.js
+++ src/templates/plugin.server.js
@@ -1,9 +1,26 @@
+<% if (options.tracing) { %>
+import { dynamicSamplingContextToSentryBaggageHeader } from '@sentry/utils'
+<% } %>
+
 /** @type {import('@nuxt/types').Module} */
 export default function (ctx, inject) {
   const sentry = process.sentry || {}
   inject('sentry', sentry)
   ctx.$sentry = sentry
-
+  <% if (options.tracing) { %>
+  const scope = ctx.$sentry.getCurrentHub().getScope()
+  if (scope) {
+    const span = scope.getSpan()
+    const transaction = scope.getTransaction()
+    if (span && transaction) {
+      ctx.app.head.meta.push({ hid: 'sentry-trace', name: 'sentry-trace', content: span.toTraceparent() })
+      const dsc = transaction.getDynamicSamplingContext()
+      if (dsc) {
+        ctx.app.head.meta.push({ hid: 'sentry-baggage', name: 'baggage', content: dynamicSamplingContextToSentryBaggageHeader(dsc) })
+      }
+    }
+  }
+  <% } %>
   <% if (options.lazy) { %>
   const sentryReady = () => Promise.resolve(sentry)
   inject('sentryReady', sentryReady)
