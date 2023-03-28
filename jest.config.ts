73fa904420dec933ca90cf5630823269351065a4
Rafal Chlodnicki <rchl2k@gmail.com>
2023-03-28 21:49:50 +0200

    fix jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,12 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
+import { JS_EXT_TO_TREAT_AS_ESM, TS_EXT_TO_TREAT_AS_ESM } from 'ts-jest'
 
 const jestConfig: JestConfigWithTsJest = {
-  preset: 'ts-jest/presets/js-with-ts-esm',
+  extensionsToTreatAsEsm: [...JS_EXT_TO_TREAT_AS_ESM, ...TS_EXT_TO_TREAT_AS_ESM],
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.m?[tj]sx?$': ['ts-jest', { useESM: true, tsconfig: './test/tsconfig-jest.json' }],
+  },
   // Crashes on CI - https://github.com/facebook/jest/issues/10662
   // collectCoverage: true,
   // collectCoverageFrom: [
