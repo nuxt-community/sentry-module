3597f0ec87b7fdc4e298c09e8ecc6037048beb00
Rafał Chłodnicki <rchl2k@gmail.com>
2023-04-05 07:25:57 -0700

    chore(ci): add bundle size checks as PR comments (#550)

---
+++ size-check/lazy+tracing/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>Server-side</h3>
+    <span id="server-side">{{ serverSentryPresent ? 'Works!' : '$sentry object is missing!' }}</span>
+    <h3>Client-side</h3>
+    <span id="client-side">{{ clientSentryPresent ? 'Works!' : '$sentry object is missing!' }}</span>
+    <p>
+      <button id="crash-button" @click="crash_me()">
+        crash me
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData ({ $sentry }) {
+    if (process.server) {
+      return {
+        serverSentryPresent: Boolean($sentry?.captureException),
+      }
+    }
+  },
+  data () {
+    return {
+      clientSentryPresent: false,
+      serverSentryPresent: false,
+    }
+  },
+  created () {
+    if (process.client) {
+      this.clientSentryPresent = Boolean(this.$sentry?.captureException)
+    }
+  },
+}
+</script>
