8ae975ec117d7391e5157c053ad25f9bfe4225b5
Rafal Chlodnicki <rchl2k@gmail.com>
2023-04-07 17:03:19 +0200

    chore(ci): don't print gzipped sizes as those fluctuate

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -5,19 +5,9 @@ module.exports = [
     gzip: false,
   },
   {
-    name: 'fixture: base (gzipped)',
-    path: 'size-check/base/.nuxt/dist/client/',
-    gzip: true,
-  },
-  {
     name: 'fixture: lazy',
     path: 'size-check/lazy/.nuxt/dist/client/',
     gzip: false,
-  },
-  {
-    name: 'fixture: lazy (gzipped)',
-    path: 'size-check/lazy/.nuxt/dist/client/',
-    gzip: true,
   },
   {
     name: 'fixture: tracing',
@@ -25,18 +15,8 @@ module.exports = [
     gzip: false,
   },
   {
-    name: 'fixture: tracing (gzipped)',
-    path: 'size-check/tracing/.nuxt/dist/client/',
-    gzip: true,
-  },
-  {
     name: 'fixture: lazy+tracing',
     path: 'size-check/lazy+tracing/.nuxt/dist/client/',
     gzip: false,
-  },
-  {
-    name: 'fixture: lazy+tracing (gzipped)',
-    path: 'size-check/lazy+tracing/.nuxt/dist/client/',
-    gzip: true,
   },
 ];
