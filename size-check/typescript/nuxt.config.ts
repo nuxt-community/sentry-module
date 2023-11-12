2910011496912e4f877915157cbd925149d50ec4
Rafal Chlodnicki <rchl2k@gmail.com>
2023-11-12 23:49:23 +0100

    path

--- size-check/typescript/nuxt.config.ts
+++ size-check/typescript/nuxt.config.ts
@@ -17,7 +17,7 @@ const config: NuxtConfig = {
     '@nuxt/typescript-build',
   ],
   modules: [
-    jiti.resolve('../../..'),
+    jiti.resolve('../..'),
   ],
   sentry: {
     dsn: 'https://fe8b7df6ea7042f69d7a97c66c2934f7@sentry.io.nuxt/1429779',
