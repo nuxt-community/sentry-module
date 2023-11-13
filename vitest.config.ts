3b01fab1bd83832828661b5528611af8779749f0
Rafał Chłodnicki <rchl2k@gmail.com>
2023-11-13 12:21:05 +0100

    chore: make tests more stable (#644)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     hookTimeout: 30000,
+    onConsoleLog (log, type) {
+      if (type === 'stderr') {
+        return false
+      }
+    },
   },
 })
